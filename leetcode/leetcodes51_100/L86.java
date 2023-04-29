f3c44ea0d6567005f76e78fc335707239f7f7705
budongbai <>
2023-04-29 09:25:04 +0800

    链表专题

--- leetcode/leetcodes51_100/L86.java
+++ leetcode/leetcodes51_100/L86.java
@@ -8,25 +8,26 @@ import helpers.ListNode;
  * @version 2017年8月3日下午10:37:43
  */
 public class L86 {
+    // 遍历链表，小于x的放small，大于x的放large，最后拼接两个链表
     public ListNode partition(ListNode head, int x) {
         if (head == null) {
-            return head;
+            return null;
         }
-        ListNode newHead = new ListNode(0), n = newHead;
-        ListNode fm = new ListNode(0), more = fm;
+        ListNode smallHead = new ListNode(0), small = smallHead;
+        ListNode largeHead = new ListNode(0), large = largeHead;
         while (head != null) {
             if (head.val < x) {
-                n.next = head;
-                n = n.next;
+                small.next = head;
+                small = small.next;
             } else {
-                more.next = head;
-                more = more.next;
+                large.next = head;
+                large = large.next;
             }
             head = head.next;
         }
-        more.next = null;
-        n.next = fm.next;
-        return newHead.next;
+        large.next = null;
+        small.next = largeHead.next;
+        return smallHead.next;
     }
 
     public static void main(String[] args) {
