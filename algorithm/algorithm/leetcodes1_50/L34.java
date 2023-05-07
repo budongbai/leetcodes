4854fe19971218f5343a6b027b1be3bfd9016e4f
baiwenxin <>
2023-05-07 19:23:30 +0800

    二分

--- algorithm/algorithm/leetcodes1_50/L34.java
+++ algorithm/algorithm/leetcodes1_50/L34.java
@@ -8,28 +8,25 @@ import java.util.Arrays;
  */
 public class L34 {
     public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) return new int[]{-1, -1};
 
-        int start = 0;
-        int end = nums.length - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] == target) {
-                int i = mid;
-                while (i >= 0 && nums[i] == target) {
-                    i--;
-                }
-                int j = mid;
-                while (j < nums.length - 1 && nums[j] == target) {
-                    j++;
-                }
-                return new int[]{++i, --j};
-            } else if (nums[mid] > target) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+        int l = 0, r = nums.length - 1; //二分范围
+        while (l < r) {                  //查找元素的开始位置
+            int mid = (l + r) / 2;
+            if (nums[mid] >= target) r = mid;
+            else l = mid + 1;
         }
-        return new int[]{-1, -1};
+        if (nums[r] != target) return new int[]{-1, -1}; //查找失败
+        int L = r;
+        l = 0;
+        r = nums.length - 1;     //二分范围
+        while (l < r) {                   //查找元素的结束位置
+
+            int mid = (l + r + 1) / 2;
+            if (nums[mid] <= target) l = mid;
+            else r = mid - 1;
+        }
+        return new int[]{L, r};
     }
 
     public int[] searchRange2(int[] A, int target) {
@@ -62,5 +59,9 @@ public class L34 {
         L34 test = new L34();
         int[] nums = {5, 7, 7, 8, 8, 10};
         System.out.println(Arrays.toString(test.searchRange(nums, 8)));
+
+
+        int[] nums2 = {2, 2};
+        System.out.println(Arrays.toString(test.searchRange(nums2, 2)));
     }
 }
